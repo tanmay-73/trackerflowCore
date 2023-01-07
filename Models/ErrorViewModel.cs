5afeee4e0503cb9bce6ed18a9e372a6cc7f68eae
Tanmay <pateltanmay73@gmail.com>
2023-01-07 18:37:42 +0530

    initials

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace trackerflowCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
